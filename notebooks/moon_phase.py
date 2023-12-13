89f5c842f09c69dfcb0d092add91cff4167301ed
micheledinelli <dinellimichele00@gmail.com>
2023-12-13 13:51:01 +0100

    feat: addded moon phase extactor and metadata reader

---
+++ notebooks/moon_phase.py
@@ -0,0 +1,27 @@
+import ephem
+
+def get_moon_phase(date):
+    # Convert the date string to an ephem Date object
+    date_obj = ephem.Date(date)
+
+    # Calculate the moon phase for the given date
+    moon = ephem.Moon(date_obj)
+    phase = moon.phase
+
+    # Define moon phase descriptions based on the phase value
+    if phase < 3.69:
+        return "New Moon"
+    elif phase < 10.19:
+        return "Waxing Crescent"
+    elif phase < 18.69:
+        return "First Quarter"
+    elif phase < 25.19:
+        return "Waxing Gibbous"
+    elif phase < 29.69:
+        return "Full Moon"
+    elif phase < 36.19:
+        return "Waning Gibbous"
+    elif phase < 43.19:
+        return "Last Quarter"
+    else:
+        return "Waning Crescent"
